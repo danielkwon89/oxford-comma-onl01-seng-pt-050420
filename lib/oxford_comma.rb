7419730b60aea5763e21ea642d750bdd51183997
Daniel Kwon <danielkwon89@gmail.com>
2020-05-10 13:04:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
   case
-  when array.size == 1
-    array[0]
-  when array.size == 2
-    array.join(" and ")
+  array[0] when array.size == 1
+  array.join(" and ") when array.size == 2
   when array.size > 2
     array[-1] = "and #{array[-1]}"
     array.join(", ")
